fca8ae6ffd0c80e8aa5fd4fd78c6d17d41cd34f5
Tux <tuxfamily.one@gmail.com>
2019-10-03 21:28:44 -0500

    06 Variables,  021 Constantes

---
+++ js/04-Constantes.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//Constantes
+
+var web = "https://victorroblesweb.es";
+const ip = "192.88.0.12";
+
+
+web = "https://victorroblescursos.es";
+//ip = "192.88.0.1";
+console.log(web, ip);
