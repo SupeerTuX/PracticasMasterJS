a340ee239b76ab63be36d78d6846f53d8a8a3c03
Tux <tuxfamily.one@gmail.com>
2019-10-04 18:06:17 -0500

    07 Tipos de datos y Operadores, 022 Operadores y tipos de datos

---
+++ js/05-TiposDatos.js
@@ -0,0 +1,19 @@
+'use strict'
+
+//Operadores
+
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
+
+alert("El resultado de la operacion es: " + operacion)
+
+
+//Tipos de datos
+var entero = 44;
+var cadena = "Hola 'que' tal";
+var booleano = false;
+
+var numeroTexto = "33";
+
+console.log(String(numeroTexto) + 7);
