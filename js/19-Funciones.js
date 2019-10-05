9ca175ede0335b613dd2f3ba78d0805047b70304
Tux <tuxfamily.one@gmail.com>
2019-10-04 20:49:35 -0500

    12 Funciones y metodos, 043 Parametros opcionales

--- js/19-Funciones.js
+++ js/19-Funciones.js
@@ -1,20 +1,27 @@
 'use strict'
 
-//Funciones
+//Parametros opcionales
 
-function calculadora(numero1, numero2) {
-    console.log("Suma: " + (numero1 + numero2));
-    console.log("Resta: " + (numero1 - numero2));
-    console.log("Multiplicacion: " + (numero1 * numero2));
-    console.log("Divicion: " + (numero1 / numero2));
-    console.log("**************************************");
+function calculadora(numero1, numero2, mostrar = false) {
+
+    if (mostrar == false) {
+        console.log("Suma: " + (numero1 + numero2));
+        console.log("Resta: " + (numero1 - numero2));
+        console.log("Multiplicacion: " + (numero1 * numero2));
+        console.log("Divicion: " + (numero1 / numero2));
+        console.log("**************************************");
+    } else {
+        document.write("Suma: " + (numero1 + numero2) + "<br/>");
+        document.write("Resta: " + (numero1 - numero2) + "<br/>");
+        document.write("Multiplicacion: " + (numero1 * numero2) + "<br/>");
+        document.write("Divicion: " + (numero1 / numero2) + "<br/>");
+        document.write("**************************************");
+    }
+
 
     //return "Hola soy calculadora!!";
 }
 
-//llamar a la funcion
 
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-    calculadora(i, 8);
-}
+calculadora(1, 4);
+calculadora(2, 5, true);
