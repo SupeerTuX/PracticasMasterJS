8355b37a70f49967696bc95f5695c91b030779ce
Tux <tuxfamily.one@gmail.com>
2019-10-04 19:43:24 -0500

    10 Alertas y ventanas, 031 Alertas y ventanas de ingreso de datos

---
+++ js/10-Ventanas.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Alerta
+//alert("Esta es mi aletra");
+
+//confirmacion
+var resultado = confirm("Estas seguro de querer continuar?");
+console.log(resultado);
+
+//Ingreso de datos
+var resultado = prompt("Que edad tienes?", 18);
+console.log(typeof resultado);
