57ac812a5e127cde892511424981e92bee1e5751
Tux <tuxfamily.one@gmail.com>
2019-10-04 21:14:43 -0500

    12 Funciones y metodos, 044 Funciones dentro de otras

--- js/19-Funciones.js
+++ js/19-Funciones.js
@@ -1,25 +1,30 @@
 'use strict'
 
 //Parametros opcionales
+function porConsola(numero1, numero2) {
+    console.log("Suma: " + (numero1 + numero2));
+    console.log("Resta: " + (numero1 - numero2));
+    console.log("Multiplicacion: " + (numero1 * numero2));
+    console.log("Divicion: " + (numero1 / numero2));
+    console.log("**************************************");
+}
+
+function porPantalla(numero1, numero2) {
+    document.write("Suma: " + (numero1 + numero2) + "<br/>");
+    document.write("Resta: " + (numero1 - numero2) + "<br/>");
+    document.write("Multiplicacion: " + (numero1 * numero2) + "<br/>");
+    document.write("Divicion: " + (numero1 / numero2) + "<br/>");
+    document.write("**************************************");
+}
 
 function calculadora(numero1, numero2, mostrar = false) {
 
     if (mostrar == false) {
-        console.log("Suma: " + (numero1 + numero2));
-        console.log("Resta: " + (numero1 - numero2));
-        console.log("Multiplicacion: " + (numero1 * numero2));
-        console.log("Divicion: " + (numero1 / numero2));
-        console.log("**************************************");
+        porConsola(numero1, numero2);
     } else {
-        document.write("Suma: " + (numero1 + numero2) + "<br/>");
-        document.write("Resta: " + (numero1 - numero2) + "<br/>");
-        document.write("Multiplicacion: " + (numero1 * numero2) + "<br/>");
-        document.write("Divicion: " + (numero1 / numero2) + "<br/>");
-        document.write("**************************************");
+        porPantalla(numero1, numero2);
     }
-
-
-    //return "Hola soy calculadora!!";
+    return true;
 }
 
 
