5f1c6e666c8bb82cacfe3bfaa43eeea4c3a18cc2
Tux <tuxfamily.one@gmail.com>
2019-10-07 16:51:02 -0500

    12 Funciones y metodos, 052 Plantillas de Texto en Javascript

--- js/23-FuncionesTexto.js
+++ js/23-FuncionesTexto.js
@@ -3,9 +3,9 @@
 //Trandormacion de textos
 
 var numero = 444;
-var texto1 = "Bienvenido al curso de JavaScript curso";
+var texto1 = "Bienvenido al curso de JavaScript";
 var texto2 = "es muy buen curso";
 
-var busqueda = texto1.includes("JavaScript");
+var busqueda = texto1.trim();
 
 console.log(busqueda);
