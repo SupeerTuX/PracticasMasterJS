e2321da518dfcbe5cfe5a8cb9481ea5d0fa8973c
Tux <tuxfamily.one@gmail.com>
2019-10-07 17:01:18 -0500

    13 Arrays y Arreglos, 053 Arrays basicos

---
+++ js/25-Arrays.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//Arrays, arreglos, matrices
+
+var nombre = "Victor Robles";
+var nombres = ["Victor Robles", "Juan Lopez", "Manolo Garcia", "Jose Martin", 52, true];
+
+var lenguajes = new Array("PHP", "JS", "Go", "Java");
+
+console.log(nombres[2]);
+console.log(lenguajes);
