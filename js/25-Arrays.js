9d7fbe03572fc47d19322d072fc53ce259fcddd2
Tux <tuxfamily.one@gmail.com>
2019-10-07 17:16:49 -0500

    13 Arrays y Arreglos, 054 Arrays avanzados

--- js/25-Arrays.js
+++ js/25-Arrays.js
@@ -7,5 +7,22 @@ var nombres = ["Victor Robles", "Juan Lopez", "Manolo Garcia", "Jose Martin", 52
 
 var lenguajes = new Array("PHP", "JS", "Go", "Java");
 
-console.log(nombres[2]);
-console.log(lenguajes);
+/*
+
+var elemento = parseInt(prompt("Que elemento del array quieres??", 0));
+
+if (elemento >= nombres.length) {
+    alert("Introduce el numero correcto menor que " + nombres.length)
+}
+else {
+    alert(nombres[elemento]);
+}
+*/
+
+document.write("<h1>Lenguajes de programacion del 2018 </h1>");
+document.write("<ul>");
+for (let index = 0; index < lenguajes.length; index++) {
+    document.write("<li>" + lenguajes[index] + "</li>");
+}
+document.write("</ul>");
+
