7ad7d2a82094a022f9c07d0effc20527d84cef53
Tux <tuxfamily.one@gmail.com>
2019-10-09 18:05:04 -0500

    18 Eventos, 071 Evento Load

--- js/30-Eventos.js
+++ js/30-Eventos.js
@@ -2,66 +2,71 @@
 
 //Eventos del raton
 
+window.addEventListener('load', () => {
 
+    function cambiarColor() {
+        var bg = boton.style.background;
+        if (bg == "green") {
+            boton.style.background = "red";
+        }
+        else {
+            boton.style.background = "green";
+        }
 
-function cambiarColor() {
-    var bg = boton.style.background;
-    if (bg == "green") {
-        boton.style.background = "red";
+        boton.style.padding = "10px";
+        boton.style.border = "1px solid #ccc";
+
+        return true;
     }
-    else {
-        boton.style.background = "green";
-    }
 
-    boton.style.padding = "10px";
-    boton.style.border = "1px solid #ccc";
+    var boton = document.querySelector("#boton");
+    boton.addEventListener('click', function () {
+        cambiarColor();
+    });
 
-    return true;
-}
+    //Mouse over
+    boton.addEventListener('mouseover', function () {
+        boton.style.background = "yellow";
+    });
 
-var boton = document.querySelector("#boton");
-boton.addEventListener('click', function () {
-    cambiarColor();
+    //Mouse out
+    boton.addEventListener('mouseout', function () {
+        boton.style.background = "#ccc";
+    });
+
+
+    //Focus
+    var input = document.querySelector("#campoNombre");
+    input.addEventListener('focus', function () {
+        boton.style.background = "#ccc";
+        console.log("[Focus]Foco en el input");
+    });
+
+    //Blur
+    input.addEventListener('blur', function () {
+        boton.style.background = "#ccc";
+        console.log("[Blur]Fuera del input");
+    });
+
+    //keydown
+    input.addEventListener('keydown', function (event) {
+        boton.style.background = "#ccc";
+        console.log("[Keydown]Pulsando la tecla", String.fromCharCode(event.keyCode));
+    });
+
+    //Keypress
+    input.addEventListener('keypress', function (event) {
+        boton.style.background = "#ccc";
+        console.log("[Keypress]Tecla presionada", String.fromCharCode(event.keyCode));
+    });
+
+    //Keyup
+    input.addEventListener('keyup', function (event) {
+        boton.style.background = "#ccc";
+        console.log("[Keyup]Tecla soltada", String.fromCharCode(event.keyCode));
+    });
+
+
 });
 
-//Mouse over
-boton.addEventListener('mouseover', function () {
-    boton.style.background = "yellow";
-});
 
-//Mouse out
-boton.addEventListener('mouseout', function () {
-    boton.style.background = "#ccc";
-});
-
-
-//Focus
-var input = document.querySelector("#campoNombre");
-input.addEventListener('focus', function () {
-    boton.style.background = "#ccc";
-    console.log("[Focus]Foco en el input");
-});
-
-//Blur
-input.addEventListener('blur', function () {
-    boton.style.background = "#ccc";
-    console.log("[Blur]Fuera del input");
-});
-
-//keydown
-input.addEventListener('keydown', function (event) {
-    boton.style.background = "#ccc";
-    console.log("[Keydown]Pulsando la tecla", String.fromCharCode(event.keyCode));
-});
-
-//Keypress
-input.addEventListener('keypress', function (event) {
-    boton.style.background = "#ccc";
-    console.log("[Keypress]Tecla presionada", String.fromCharCode(event.keyCode));
-});
-
-//Keyup
-input.addEventListener('keyup', function (event) {
-    boton.style.background = "#ccc";
-    console.log("[Keyup]Tecla soltada", String.fromCharCode(event.keyCode));
-});
