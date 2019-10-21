1b33276e414420ed8aed40cd982f39b37d5dad75
Tux <tuxfamily.one@gmail.com>
2019-10-21 18:20:17 -0500

    28 Efectos animaciones y mas, 103 Animaciones personalizadas

--- jquery/js/04-Efectos.js
+++ jquery/js/04-Efectos.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    caja = $('#caja');
     /*
        $('#mostrar').hide();
    
@@ -15,6 +17,27 @@ $(document).ready(function () {
        });
    */
     $('#toggle').click(function () {
-        $('#caja').slideToggle('fast');
+        caja.slideToggle('fast');
     });
+
+    $('#animar').click(function () {
+        caja.animate({
+            marginLeft: '500px',
+            fontSize: '40px',
+            height: '110px'
+        }, 'slow')
+            .animate({
+                borderRadius: '900px',
+                marginTop: '80px'
+            }, 'slow')
+            .animate({
+                borderRadius: '0px',
+                marginLeft: '0px'
+            }, 'slow')
+            .animate({
+                borderRadius: '100px',
+                marginTop: '0px'
+            }, 'slow');
+    });
+
 });
