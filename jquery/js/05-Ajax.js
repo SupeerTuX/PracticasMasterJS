23374974de6b52ac98961d05266201b077f93b06
Tux <tuxfamily.one@gmail.com>
2019-10-23 15:57:26 -0500

    29 Peticiones AJAX en jQuery, 106 Metodo Get - AJAX

--- jquery/js/05-Ajax.js
+++ jquery/js/05-Ajax.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
     //Load
-    $('#datos').load('https://reqres.in');
+    //$('#datos').load('https://reqres.in');
+
+    //Get y Post
+    $.get("https://reqres.in/api/users", { page: 2 }, function (response) {
+        response.data.forEach((element, index) => {
+            console.log(element.first_name);
+            $('#datos').append("<p>" + element.first_name + "</p>");
+        });
+    });
+
 
 });
