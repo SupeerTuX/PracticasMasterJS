4a4b439661de3138a7f2dc9b7a0a39c7f548c6d6
Tux <tuxfamily.one@gmail.com>
2019-10-23 17:57:08 -0500

    30 Primeros pasos con jQuery UI, 110 Incluir jQuery UI

--- jquery/js/05-Ajax.js
+++ jquery/js/05-Ajax.js
@@ -19,13 +19,32 @@ $(document).ready(function () {
             name: $('input[name="name"]').val(),
             web: $('input[name="web"]').val()
         };
-        console.log(usuario);
 
-        $.post($(this).attr("action"), usuario, function (response) {
-            console.log(response);
-        }).done(function () {
-            alert('Usuario añadido correctamente');
+        /*
+                $.post($(this).attr("action"), usuario, function (response) {
+                    console.log(response);
+                }).done(function () {
+                    alert('Usuario añadido correctamente');
+                });
+        */
+
+        $.ajax({
+            type: 'POST',
+            url: $(this).attr("action"),
+            data: usuario,
+            beforeSend: function () {
+                console.log('Enviando usuario...');
+            },
+            success: function (response) {
+                console.log(response);
+            },
+            error: function () {
+                console.log('A ocurrido un error');
+            },
+            timeout: 2000
+
         });
+
         return false;
     });
 
