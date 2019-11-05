046b657148bfc63fc4bc4af0c3cc2334202c53c6
Tux <tuxfamily.one@gmail.com>
2019-11-04 19:46:43 -0600

    37 Funcionalidades avanzadas, 132 Listado dinamico de articulos

--- ProyectoJS/js/main.js
+++ ProyectoJS/js/main.js
@@ -1,9 +1,59 @@
 $(document).ready(function () {
 
+    //Slider
     $('.bxslider').bxSlider({
         mode: 'fade',
         captions: false,
         slideWidth: 1200,
         responsive: true
+    });
+
+    //Posts
+    var posts = [
+        {
+            title: 'Prieba de titulo1',
+            date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus blandit metus. Ut ante leo, tempor condimentum mollis ac, egestas non mauris. Aliquam erat volutpat. Duis fringilla tempus ligula id finibus. In cursus faucibus fringilla. Duis consectetur elit quis risus dignissim commodo. Nunc ut vestibulum quam. Proin turpis est, imperdiet vitae consectetur a, viverra in felis. Suspendisse elementum mattis faucibus. Aenean non egestas dolor, vel tincidunt nibh. Maecenas volutpat eget nisl sit amet ornare.'
+
+        },
+        {
+            title: 'Prieba de titulo2',
+            date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus blandit metus. Ut ante leo, tempor condimentum mollis ac, egestas non mauris. Aliquam erat volutpat. Duis fringilla tempus ligula id finibus. In cursus faucibus fringilla. Duis consectetur elit quis risus dignissim commodo. Nunc ut vestibulum quam. Proin turpis est, imperdiet vitae consectetur a, viverra in felis. Suspendisse elementum mattis faucibus. Aenean non egestas dolor, vel tincidunt nibh. Maecenas volutpat eget nisl sit amet ornare.'
+
+        },
+        {
+            title: 'Prieba de titulo3',
+            date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus blandit metus. Ut ante leo, tempor condimentum mollis ac, egestas non mauris. Aliquam erat volutpat. Duis fringilla tempus ligula id finibus. In cursus faucibus fringilla. Duis consectetur elit quis risus dignissim commodo. Nunc ut vestibulum quam. Proin turpis est, imperdiet vitae consectetur a, viverra in felis. Suspendisse elementum mattis faucibus. Aenean non egestas dolor, vel tincidunt nibh. Maecenas volutpat eget nisl sit amet ornare.'
+
+        },
+        {
+            title: 'Prieba de titulo4',
+            date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus blandit metus. Ut ante leo, tempor condimentum mollis ac, egestas non mauris. Aliquam erat volutpat. Duis fringilla tempus ligula id finibus. In cursus faucibus fringilla. Duis consectetur elit quis risus dignissim commodo. Nunc ut vestibulum quam. Proin turpis est, imperdiet vitae consectetur a, viverra in felis. Suspendisse elementum mattis faucibus. Aenean non egestas dolor, vel tincidunt nibh. Maecenas volutpat eget nisl sit amet ornare.'
+
+        },
+        {
+            title: 'Prieba de titulo5',
+            date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format("MMMM") + ' del ' + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus blandit metus. Ut ante leo, tempor condimentum mollis ac, egestas non mauris. Aliquam erat volutpat. Duis fringilla tempus ligula id finibus. In cursus faucibus fringilla. Duis consectetur elit quis risus dignissim commodo. Nunc ut vestibulum quam. Proin turpis est, imperdiet vitae consectetur a, viverra in felis. Suspendisse elementum mattis faucibus. Aenean non egestas dolor, vel tincidunt nibh. Maecenas volutpat eget nisl sit amet ornare.'
+
+        }
+
+    ];
+
+    posts.forEach((item, index) => {
+        var post = `
+        <article class="post">
+                    <h2>${item.title}</h2>
+                    <span class="date">${item.date}</span>
+                    <p>${item.content}</p>
+                    <a href="#" class="buttonMore">Leer mas</a>
+                </article>
+        `;
+
+        $("#posts").append(post);
+
     });
 });
